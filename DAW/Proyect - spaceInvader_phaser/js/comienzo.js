763faacc824907231523df2ff37128b3e04914bf
samuel <sam@example.com>
2018-02-22 17:01:22 +0000

    files removed

--- DAW/Proyect - spaceInvader_phaser/js/comienzo.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'escenario'); 
-
-game.state.add("Menu", Menu);
-game.state.add("Juego", Juego);
-game.state.add("gameOver", gameOver);
-
-game.state.start("Menu");
